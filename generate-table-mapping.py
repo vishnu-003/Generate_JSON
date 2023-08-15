51be775c11a79851449236757adbab159b104776
vishnu-003 <svishnuvardhan1209@gmail.com>
2023-08-15 23:00:27 +0530

    Added rule values

--- generate-table-mapping.py
+++ generate-table-mapping.py
@@ -12,7 +12,7 @@ def replace_placeholders(input_file, output_file, replacements):
 
         with open(output_file, 'w') as f:
             f.write(modified_content)
-        
+      
         print("Replacement successful. Modified content saved to", output_file)
     except Exception as e:
         print("An error occurred:", e)
@@ -21,12 +21,13 @@ with open(schemas_file) as file:
     for i, line in enumerate(file):
         schema_name, table_name = line.strip().split(",")
         output_filename = f"{table_mapping_out}/table-mappings-{schema_name}.json"
-        
+        RULE_ID=2010100 + i
         replacements = {
             "#SCHEMA_NAME#": schema_name,
             "#TABLE_NAME#": table_name,
-            "#RULE_ID#": str(2010100 + i),  # Increment i to get unique rule-id
-            "#RULE_NAME#": str(2010100 + i)  # Increment i to get unique rule-name
+            "#RULE_ID#":str(RULE_ID),  # Increment i to get unique rule-id
+            "#RULE_NAME#": str(RULE_ID)  # Increment i to get unique rule-name
+            
         }
         
         replace_placeholders(table_mapping_template, output_filename, replacements)
